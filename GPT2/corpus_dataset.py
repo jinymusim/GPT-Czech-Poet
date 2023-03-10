855e92382e66e12116f9501907381afaa0db3352
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-03-10 12:22:10 +0100

    Dataset Loader, But is too much for Mem

--- GPT2/corpus_dataset.py
+++ GPT2/corpus_dataset.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import json
 import numpy as np
+from typing import Any
 import tensorflow as tf
 
 class CorpusDataLoad:
@@ -8,10 +10,30 @@ class CorpusDataLoad:
     
     class Dataset:
         
-        def __init__(self, data: np.ndarray, shuffle_bool:bool= True, seed:int= 42):
-            self._data = data
+        def __init__(self, data, shuffle_bool:bool= True, seed:int= 42):
+            self._raw_data = data
+            self._size = len(self._raw_data)
+            
+        @property
+        def size(self):
+            return self._size
     
-    def __init__(self, data_dir):
+        @property
+        def raw_data(self):
+            return self._raw_data
+            
+    def load_jsons(self):
+        data_filenames = os.listdir(self.data_dir)
+        data_by_files = []
+        for filename in data_filenames:
+            file_path = os.path.join(self.data_dir, filename)
+            with open(file_path, 'r') as file:
+               data_by_files.append(json.load(file))
+        self.data = CorpusDataLoad.Dataset(data_by_files)
+    
+    def __init__(self, data_dir = "GPT2\corpusCzechVerse-master\ccv"):
         self.data_dir = data_dir
+        self.load_jsons()
         
-    
+train_dat = CorpusDataLoad()
+print(train_dat.data.size())
