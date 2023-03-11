d8b1bf90141be196a4183361552a12ee93f43151
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-03-11 22:45:37 +0100

    Problems with sequence length

--- GPT2/gpt2_finetune.py
+++ GPT2/gpt2_finetune.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 MODEL_PATH = "./gpt2-cz-poetry" 
 
 tokenizer = GPT2Tokenizer.from_pretrained("lchaloupsky/czech-gpt2-oscar")
+tokenizer.model_max_length=1024
 model = TFGPT2LMHeadModel.from_pretrained("lchaloupsky/czech-gpt2-oscar")
 
 train_data = CorpusDataLoad(tokenizer)
@@ -14,8 +15,9 @@ model.compile(
     loss= tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE),
     metrics=['accuracy']
 )
-tf_dataset = train_data.dataset.dataset_body.shuffle(128).padded_batch(4, padded_shapes=([None],[None]))
+#tf_dataset_text = train_data.dataset.dataset_text.shuffle(128).padded_batch(4, padded_shapes=([None],[None]))
+tf_dataset_body = train_data.dataset.dataset_body.shuffle(64).padded_batch(4, padded_shapes=([None],[None]))
 
-model.fit(tf_dataset, epochs=1)
+model.fit(tf_dataset_body, epochs=1)
 
 model.save_model(MODEL_PATH)
