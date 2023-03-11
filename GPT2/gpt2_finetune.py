be5626f7ad8413617725f00d9593d713e3970108
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-03-11 19:29:09 +0100

    Little learning

--- GPT2/gpt2_finetune.py
+++ GPT2/gpt2_finetune.py
@@ -2,6 +2,8 @@ from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
 from corpus_dataset import CorpusDataLoad
 import tensorflow as tf
 
+MODEL_PATH = "./gpt2-cz-poetry" 
+
 tokenizer = GPT2Tokenizer.from_pretrained("lchaloupsky/czech-gpt2-oscar")
 model = TFGPT2LMHeadModel.from_pretrained("lchaloupsky/czech-gpt2-oscar")
 
@@ -12,5 +14,8 @@ model.compile(
     loss= tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE),
     metrics=['accuracy']
 )
+tf_dataset = train_data.dataset.dataset_body.shuffle(128).padded_batch(4, padded_shapes=([None],[None]))
 
-model.fit(train_data.dataset.dataset_body, epochs=1)
+model.fit(tf_dataset, epochs=1)
+
+model.save_model(MODEL_PATH)
