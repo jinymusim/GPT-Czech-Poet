6917461a1ac7faa6561ed5169b9786b8cdc0c422
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-03-11 08:39:40 +0100

    Padded Tensors with proper dims

--- GPT2/corpus_dataset.py
+++ GPT2/corpus_dataset.py
@@ -32,7 +32,7 @@ class CorpusDataLoad:
                 for data_line in datum:
                     for part_line in data_line['body']:
                         for text_line in part_line:
-                            yield self._tokenizer.encode(text_line['text'], return_tensors="tf")
+                            yield self._tokenizer.encode(text_line['text'], return_tensors="tf")[0]
                             
         def data_part_gen(self):
             for filename in self._data_file_paths:
@@ -45,7 +45,7 @@ class CorpusDataLoad:
                         for text_line in part_line:
                             body.append(text_line['text'])
                         part.append("\n".join(body))
-                    yield self._tokenizer.encode("\n".join(part), return_tensors="tf")
+                    yield self._tokenizer.encode("\n".join(part), return_tensors="tf")[0]
                     
         def data_body_gen(self):
             for filename in self._data_file_paths:
@@ -56,19 +56,19 @@ class CorpusDataLoad:
                         body = []
                         for text_line in part_line:
                             body.append(text_line['text'])
-                        yield self._tokenizer.encode("\n".join(body), return_tensors="tf")
+                        yield self._tokenizer.encode("\n".join(body), return_tensors="tf")[0]
         
         @property
         def dataset_text(self) -> tf.data.Dataset:
-            return tf.data.Dataset.from_generator(self.data_text_line_gen, output_signature=tf.TensorSpec(shape=(1,None),dtype=tf.int32))
+            return tf.data.Dataset.from_generator(self.data_text_line_gen, output_signature=tf.TensorSpec(shape=(None),dtype=tf.int32))
         
         @property
         def dataset_part(self) -> tf.data.Dataset:
-            return tf.data.Dataset.from_generator(self.data_part_gen, output_signature=tf.TensorSpec(shape=(1,None), dtype=tf.int32))
+            return tf.data.Dataset.from_generator(self.data_part_gen, output_signature=tf.TensorSpec(shape=(None), dtype=tf.int32))
         
         @property
         def dataset_body(self) -> tf.data.Dataset:
-            return tf.data.Dataset.from_generator(self.data_body_gen, output_signature=tf.TensorSpec(shape=(1,None),dtype=tf.int32))
+            return tf.data.Dataset.from_generator(self.data_body_gen, output_signature=tf.TensorSpec(shape=(None),dtype=tf.int32))
         
 
             
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     train_dat = CorpusDataLoad(tokenizer)
     
     tf_dataset = train_dat.dataset.dataset_body
-    tf_dataset = tf_dataset.shuffle(128).padded_batch(8)
+    tf_dataset = tf_dataset.shuffle(128).padded_batch(8, padded_shapes=([None]))
     i = 0
     for datum in tf_dataset.enumerate():
         if i >= 10:
