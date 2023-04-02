5d2f1c26d2865296e622e4c24c40b358fc1f02ce
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-04-02 10:18:31 +0200

    Start of dialog system

---
+++ DialogSystem/finetune_torch.py
@@ -0,0 +1,47 @@
+import transformers
+from transformers import  AutoTokenizer, AutoModelForCausalLM
+import torch
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--seed", default=42, type=int, help="Seed to set for Torch")
+parser.add_argument("--batch_size", default=4,  type=int, help="Batch size")
+parser.add_argument("--epochs", default=10, type=int, help="Number of Epochs to finetune")
+parser.add_argument("--learning_rate", default=1e-5, type=float, help="Learning rate for finetune")
+parser.add_argument("--max_token_len", default=1024, type=int, help="Max length for tokenizer")
+parser.add_argument("--use_default_model", default=True, type=bool, help="Bool if default huggingface model used")
+parser.add_argument("--default_hf_model", default="lchaloupsky/czech-gpt2-oscar", type=str, help="Default huggingface model path")
+parser.add_argument("--model_path", default=os.path.abspath(os.path.join(os.path.dirname("__file__"), "dialog.model")), type=str, help="Model path")
+parser.add_argument("--half_precision", default=True, type=bool, help="Use half precision on model")
+parser.add_argument("--use_gpu_if_available", default=False, type=bool, help="If GPU should be used")
+
+def main(args: argparse.Namespace):
+    # Base Device is CPU
+    device = torch.device('cpu')
+    # If Wanted and GPU is available, use it
+    if args.use_gpu_if_available:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Use default HF Model if requested
+    if args.use_default_model:
+        tokenizer = AutoTokenizer.from_pretrained(args.default_hf_model)
+        model = AutoModelForCausalLM.from_pretrained(args.default_hf_model)
+    else: # Use Own Model if default not used
+        tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+        model = AutoModelForCausalLM.from_pretrained(args.model_path)
+    
+    tokenizer.model_max_length = args.max_token_len
+    # Move Model to desired Device
+    model = model.to(device)
+    optimizer = torch.optim.AdamW(model.params(), lr=args.learning_rate)
+    scheduler = transformers.get_cosine_schedule_with_warmup(optimizer, 1000//args.batch_size, 1000 * args.epochs // args.batch_size)
+    
+    
+    # Save Model
+    model.save_pretrained(args.model_path)
+              
+        
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
---
+++ DialogSystem/trainer_torch.py
@@ -0,0 +1,42 @@
+import transformers
+from transformers import PreTrainedModel
+import torch
+from torch.optim import Optimizer, lr_scheduler
+import os
+import sys
+
+class Trainer:
+    
+    def __init__(self, model: PreTrainedModel, 
+                 epochs: int, optimizer: Optimizer, 
+                 scheduler: lr_scheduler._LRScheduler,
+                 dataloader) -> None:
+        self.model = model
+        self.epochs = epochs
+        self.device = model.device
+        self.optimizer = optimizer
+        self.scheduler =scheduler
+        self.dataloader = dataloader
+        
+    def train(self):      
+        for epoch in range(self.epochs):
+            
+            self.model.train()
+            
+            for step, batch in enumerate(self.dataloader):
+                
+                inputs = batch['input']
+                labels = batch['label']
+                
+                self.optimizer.zero_grad()
+                out = self.model(input_ids=inputs.to(self.device), labels=labels.to(self.device))
+                out.loss.backward()
+                self.optimizer.step()
+                    
+                self.scheduler.step()
+                
+                output = {'loss' : out.loss.item()}
+                if step % 500 == 0:
+                    print(f'Step {step},  loss : {output["loss"]}')
+                
+            
