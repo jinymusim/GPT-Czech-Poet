2be55a18f18bcf4d85df58d8c0b2dbc7056bd6c6
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-04-02 11:49:46 +0200

    Basic framework for dataloading

---
+++ DialogSystem/finetune_dataloader.py
@@ -0,0 +1,26 @@
+import os
+import torch
+import datasets
+import pickle
+import json
+
+class DialogDataset:
+    
+    def __init__(self, dataset, split,cache_dir='./') -> None:
+        self.dataset = dataset
+        self.split = split
+        self.file_name = os.path.join(cache_dir, f"{split}_data.json")
+        if os.path.isfile(self.file_name):
+            data = pickle.load(open(self.file_name), 'rb')
+        else:
+            dataset = datasets.load_dataset(path=dataset, split=split, ignore_verifications=True, streaming=True)
+            data = []
+            for idx, dialogue in enumerate(dataset):
+                if idx % 500 == 0:
+                    print(f'Processing dialogue {idx + 1}')
+                data.extend(self.parse(dialogue))
+            pickle.dump(data, open(self.file_name, 'wb+'))
+        self.data = data
+                
+    def parse(self, dialogue):
+        pass
