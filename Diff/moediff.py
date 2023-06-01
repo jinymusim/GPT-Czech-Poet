a63dff4e22826606dbb3b9c11452918d24e2cff9
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-06-01 21:48:55 +0200

    longer

--- Diff/moediff.py
+++ Diff/moediff.py
@@ -8,7 +8,7 @@ import torch
 # "Ojimi/anime-kawai-diffusion"
 # "dreamlike-art/dreamlike-anime-1.0"
 # "FredZhang7/anime-anything-promptgen-v2"
-pipe = StableDiffusionPipeline.from_pretrained("dreamlike-art/dreamlike-anime-1.0", safety_checker=None, torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained("Ojimi/anime-kawai-diffusion", safety_checker=None, torch_dtype=torch.float16)
 pipe.enable_model_cpu_offload()
 i= 0
 
@@ -31,18 +31,18 @@ i= 0
 # More Fantasy Like
 # "1girl, boobs, boob armor, six pack, battle scars, sweat, some colors, more colors, high quality, highest quality, masterpiece"
 # "1girl, boobs, boob armor, six pack, blood, sweat, some colors, more colors, high quality, highest quality, masterpiece"
-possible_prompts = ["1girl, lingerie, garter belt, dim lighting, dim light, realistic, highest quality, masterpiece",
+possible_prompts = ["1girl, garter belt, dim lighting, dim light, realistic, highest quality, masterpiece",
                     "1girl, underwear, underboob, colorful, realistic, highest quality, masterpiece",
                     "1girl, boobs, boob armor, six pack, blood, sweat, some colors, more colors, high quality, highest quality, masterpiece",
-                    "1women, mature women, wide hips, color clothes",]
+                    "1girl, six pack, blood, colorful, high quality, highest quality, masterpiece",]
 
-while i< 25:  
+while i< 50:  
     prompt = random.choice(possible_prompts)
     
     image = pipe(prompt, negative_prompt="""simple background, retro style, low quality, lowest quality, 
                  1980s, 1990s, 2000s, bad anatomy, bad proportions, lowres, username, artist name, 
                  error, duplicate, watermark, signature, text, extra digit, fewer digits, worst quality, jpeg artifacts, blurry""",
-                 num_inference_steps=1000).images[0]
+                 num_inference_steps=200).images[0]
     image.save(prompt + f"{i}.png")
     
     i+=1
