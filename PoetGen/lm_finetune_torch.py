8732ddf8ca2c9af59002c88cb32ea019d67c8586
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-07-16 09:51:20 +0200

    More  emphasis on verses

--- PoetGen/lm_finetune_torch.py
+++ PoetGen/lm_finetune_torch.py
@@ -11,9 +11,9 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--batch_size_LM", default=8, type=int, help="Batch size.")
-parser.add_argument("--epochs_LM", default=6, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_LM", default=2, type=int, help="Number of epochs to run.")
 parser.add_argument("--batch_size_poet", default=4, type=int, help="Batch size.")
-parser.add_argument("--epochs_poet", default=16, type=int, help="Number of epochs for poet gen")
+parser.add_argument("--epochs_poet", default=20, type=int, help="Number of epochs for poet gen")
 
 parser.add_argument("--learning_rate", default=1e-5, type=float, help="Learning Rate for Finetuning")
 parser.add_argument("--data_path",  default=os.path.abspath(os.path.join(os.path.dirname(__file__), "corpusCzechVerse", "ccv")), type=str, help="Path to Data")
