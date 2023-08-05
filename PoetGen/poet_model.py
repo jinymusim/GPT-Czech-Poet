9b1e0451d194d11dab9a4556a0370bcc8f6ca890
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-08-05 09:50:31 +0200

    no device map

--- PoetGen/poet_model.py
+++ PoetGen/poet_model.py
@@ -15,7 +15,6 @@ class PoetModel(torch.nn.Module):
         if "llama" in pretrainedModel:
             self.model = AutoModelForCausalLM.from_pretrained(pretrainedModel, 
                                                               output_hidden_states=True, 
-                                                              device_map={'': 0},
                                                               max_memory = {i: torch.cuda.mem_get_info(i)[0] for i in range(torch.cuda.device_count())}, 
                                                               torch_dtype=torch.float16)
         else:
