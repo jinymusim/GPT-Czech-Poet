fe741615e8581ce111a1f6ed47efed139e1d36cc
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-08-06 20:15:01 +0200

    Newer pip

--- PoetGen/train_poet_gpt.sh
+++ PoetGen/train_poet_gpt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q gpu -l select=1:ncpus=1:ngpus=1:mem=24gb:scratch_local=16gb
 #PBS -l walltime=24:00:00
-module add py-pip/py-pip-19.3-intel-19.0.4-hudzomi
+module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
 singularity shell --nv /cvmfs/singularity.metacentrum.cz/NGC/PyTorch:22.10-py3.SIF
 cd $SCRATCHDIR
