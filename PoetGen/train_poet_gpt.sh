e43e5d545a183a57364d9e55e5fccc75badbd1ef
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-08-06 20:34:15 +0200

    Newer pytorch

--- PoetGen/train_poet_gpt.sh
+++ PoetGen/train_poet_gpt.sh
@@ -3,7 +3,7 @@
 #PBS -l walltime=24:00:00
 module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
-singularity shell --nv /cvmfs/singularity.metacentrum.cz/NGC/PyTorch:22.10-py3.SIF
+singularity shell --nv /cvmfs/singularity.metacentrum.cz/NGC/PyTorch:23.05-py3.SIF
 cd $SCRATCHDIR
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
