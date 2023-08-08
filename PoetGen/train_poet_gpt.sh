cf3befaa6015f24f4fdef5f37f370b4cfb39207d
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-08-08 15:07:27 +0200

    With import cuda, signature not needed

--- PoetGen/train_poet_gpt.sh
+++ PoetGen/train_poet_gpt.sh
@@ -3,7 +3,6 @@
 #PBS -l walltime=24:00:00
 module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
-singularity shell --nv /cvmfs/singularity.metacentrum.cz/NGC/PyTorch:23.05-py3.SIF
 cd $SCRATCHDIR
 pip install --target=$SCRATCHDIR torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
