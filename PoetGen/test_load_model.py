ee8db6e757cc24ffcd7dcdb0e6b860bd51119efa
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-09-24 18:02:31 +0200

    very long + validtaion

--- PoetGen/test_load_model.py
+++ PoetGen/test_load_model.py
@@ -7,7 +7,7 @@ from poet_model_interface import PoetModelInterface
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--model_path_full", default=os.path.abspath(os.path.join(os.path.dirname(__file__),'backup_LMS', "gpt-cz-poetry-all_tasks_e4_e16")),  type=str, help="Path to Model")
+parser.add_argument("--model_path_full", default=os.path.abspath(os.path.join(os.path.dirname(__file__),'backup_LMS', "gpt-cz-poetry-all_tasks_e8_e32")),  type=str, help="Path to Model")
 # bigscience/bloom-560m
 parser.add_argument("--default_hf_model", default="lchaloupsky/czech-gpt2-oscar", type=str, help="Default Model from HF to use")
 parser.add_argument("--result_file", default= os.path.abspath(os.path.join(os.path.dirname(__file__),'results', "test_poet_model.txt")), type=str, help="Where to store the decoding efforts")
