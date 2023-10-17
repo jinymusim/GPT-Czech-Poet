4af06bdd012aba96cc273bd1a351582db718431e
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-10-17 22:27:35 +0200

    some other

--- PoetGen/train_validator.py
+++ PoetGen/train_validator.py
@@ -36,13 +36,13 @@ parser.add_argument("--syllables", default=False, type=bool, help="If to use syl
 
 parser.add_argument("--block_count", default=3, type=int, help="Max length for tokenizer")
 parser.add_argument("--n_embd_metre", default=512, type=int, help="Max length for tokenizer")
-parser.add_argument("--batch_size_metre", default=32, type=int, help="Batch size.")
+parser.add_argument("--batch_size_metre", default=64, type=int, help="Batch size.")
 parser.add_argument("--epochs_metre", default=64, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--hidden_layers", default=3, type=int, help="Max length for tokenizer")
 parser.add_argument("--hidden_layer_rhyme", default=1024, type=int, help="Max length for tokenizer")
-parser.add_argument("--batch_size_rhyme", default=32, type=int, help="Batch size.")
-parser.add_argument("--epochs_rhyme", default=512, type=int, help="Number of epochs to run.")
+parser.add_argument("--batch_size_rhyme", default=512, type=int, help="Batch size.")
+parser.add_argument("--epochs_rhyme", default=256, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--lower_case", default=True, type=bool, help="If to lower case data")
 parser.add_argument("--val_data_rate", default=0.1, type=float, help="Rate of validation data")
