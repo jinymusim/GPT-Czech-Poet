3fb6b04a77ddb9fe60b4214991af678939cdafe1
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-10-28 21:43:12 +0200

    long SAM

--- PoetGen/train_validator.py
+++ PoetGen/train_validator.py
@@ -35,10 +35,10 @@ parser.add_argument("--syllables", default=True, type=bool, help="If to use syll
 
 parser.add_argument("--pretrained_model", default="roberta-base", type=str, help="Roberta Model")
 parser.add_argument("--batch_size_metre", default=64, type=int, help="Batch size.")
-parser.add_argument("--epochs_metre", default=16, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_metre", default=1, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--batch_size_rhyme", default=64, type=int, help="Batch size.")
-parser.add_argument("--epochs_rhyme", default=16, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_rhyme", default=32, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--lower_case", default=True, type=bool, help="If to lower case data")
 parser.add_argument("--val_data_rate", default=0.05, type=float, help="Rate of validation data")
