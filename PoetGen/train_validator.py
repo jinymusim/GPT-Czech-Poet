18aff14007a02ee4f6521c42862fb268898e05b9
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-05 14:24:56 +0100

    This shouldn't need syllable

--- PoetGen/train_validator.py
+++ PoetGen/train_validator.py
@@ -33,7 +33,7 @@ parser.add_argument("--prompt_rhyme", default=True, type=bool, help="Rhyme is pr
 parser.add_argument("--prompt_length", default=True, type=bool, help="Verse length is prompted into training data")
 parser.add_argument("--prompt_ending", default=True, type=bool, help="Ending of Verse is prompted into training data")
 
-parser.add_argument("--syllables", default=True, type=bool, help="If to use syllable data")
+parser.add_argument("--syllables", default=False, type=bool, help="If to use syllable data")
 
 parser.add_argument("--pretrained_model", default="roberta-base", type=str, help="Roberta Model")
 
@@ -41,7 +41,7 @@ parser.add_argument("--batch_size_metre", default=64, type=int, help="Batch size
 parser.add_argument("--epochs_metre", default=0, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--batch_size_rhyme", default=64, type=int, help="Batch size.")
-parser.add_argument("--epochs_rhyme", default=4, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_rhyme", default=0, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--batch_size_year", default=64, type=int, help="Batch size.")
 parser.add_argument("--epochs_year", default=4, type=int, help="Number of epochs to run.")
