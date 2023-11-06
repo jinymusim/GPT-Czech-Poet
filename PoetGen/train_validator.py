a56f38b2807cb4fae0bd48f4ca94b9696641e069
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-06 10:09:06 +0100

    Validator

--- PoetGen/train_validator.py
+++ PoetGen/train_validator.py
@@ -38,10 +38,10 @@ parser.add_argument("--syllables", default=True, type=bool, help="If to use syll
 parser.add_argument("--pretrained_model", default="ufal/robeczech-base", type=str, help="Roberta Model")
 
 parser.add_argument("--batch_size_metre", default=64, type=int, help="Batch size.")
-parser.add_argument("--epochs_metre", default=0, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_metre", default=64, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--batch_size_rhyme", default=64, type=int, help="Batch size.")
-parser.add_argument("--epochs_rhyme", default=64, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_rhyme", default=0, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--batch_size_year", default=64, type=int, help="Batch size.")
 parser.add_argument("--epochs_year", default=0, type=int, help="Number of epochs to run.")
