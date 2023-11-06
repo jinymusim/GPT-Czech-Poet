6f7243bce6c819955e39ad7cab2f33c3645a2de8
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-06 09:31:44 +0100

    Roberczech long

--- PoetGen/train_validator.py
+++ PoetGen/train_validator.py
@@ -24,7 +24,7 @@ parser.add_argument("--learning_rate_year", default=5e-5, type=float, help="Lear
 
 parser.add_argument("--data_path",  default=os.path.abspath(os.path.join(os.path.dirname(__file__), "corpusCzechVerse", "ccv")), type=str, help="Path to Data")
 #parser.add_argument("--tokenizer", default=os.path.abspath(os.path.join(os.path.dirname(__file__), "utils", "tokenizers", "BPE", "syllabs_processed_tokenizer.json")), type=str, help="Tokenizer to use")
-parser.add_argument("--tokenizer", default="roberta-base", type=str, help="Tokenizer to use")
+parser.add_argument("--tokenizer", default="ufal/robeczech-base", type=str, help="Tokenizer to use")
 parser.add_argument("--model_path", default=os.path.abspath(os.path.join(os.path.dirname(__file__), "utils", "validators")),  type=str, help="Path to Model")
 parser.add_argument("--max_len", default=512, type=int, help="Max length for tokenizer")
 parser.add_argument("--verse_len", default=[4,6], type=list, help="Lengths of verses")
@@ -35,16 +35,16 @@ parser.add_argument("--prompt_ending", default=True, type=bool, help="Ending of 
 
 parser.add_argument("--syllables", default=True, type=bool, help="If to use syllable data")
 
-parser.add_argument("--pretrained_model", default="roberta-base", type=str, help="Roberta Model")
+parser.add_argument("--pretrained_model", default="ufal/robeczech-base", type=str, help="Roberta Model")
 
 parser.add_argument("--batch_size_metre", default=64, type=int, help="Batch size.")
 parser.add_argument("--epochs_metre", default=0, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--batch_size_rhyme", default=64, type=int, help="Batch size.")
-parser.add_argument("--epochs_rhyme", default=16, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_rhyme", default=64, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--batch_size_year", default=64, type=int, help="Batch size.")
-parser.add_argument("--epochs_year", default=16, type=int, help="Number of epochs to run.")
+parser.add_argument("--epochs_year", default=0, type=int, help="Number of epochs to run.")
 
 parser.add_argument("--lower_case", default=True, type=bool, help="If to lower case data")
 parser.add_argument("--val_data_rate", default=0.05, type=float, help="Rate of validation data")
