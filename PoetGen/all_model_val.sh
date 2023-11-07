c615e9744c2d10df2dda5bd91c72035491a96c67
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-07 21:48:08 +0100

    maybe now

--- PoetGen/all_model_val.sh
+++ PoetGen/all_model_val.sh
@@ -1,15 +1,15 @@
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/Base-Tokenizer-NormalText-gpt-cz-poetry-base-e4e16_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/Base-Tokenizer-NormalText-gpt-cz-poetry-all-e1e24_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/Base-Tokenizer-NormalText-gpt-cz-poetry-all-e4e16_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-base-e8e24_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-base-e8e16_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-base-e4e8_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-all-e4e8_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e8e16_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e4e8_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e4e4_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e2e4_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-all-e4e16_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Processed-BPE-NormalText-gpt-cz-poetry-base-e8e16_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Processed-BPE-NormalText-gpt-cz-poetry-all-e4e4_LM
-qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- ./all_model_val_helper.sh ./backup_LMS/New-Processed-BPE-NormalText-gpt-cz-poetry-all-e4e16_LM
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/Base-Tokenizer-NormalText-gpt-cz-poetry-base-e4e16_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/Base-Tokenizer-NormalText-gpt-cz-poetry-all-e1e24_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/Base-Tokenizer-NormalText-gpt-cz-poetry-all-e4e16_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-base-e8e24_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-base-e8e16_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-base-e4e8_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/Unicode-Tokenizer-NormalText-gpt-cz-poetry-all-e4e8_LM] ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e8e16_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e4e8_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e4e4_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-base-e2e4_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Syllable-BPE-NormalText-gpt-cz-poetry-all-e4e16_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Processed-BPE-NormalText-gpt-cz-poetry-base-e8e16_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Processed-BPE-NormalText-gpt-cz-poetry-all-e4e4_LM ]
+qsub -N ModelValidation -q default -l select=1:ncpus=1:mem=24gb:scratch_local=20gb -l walltime=48:00:00 -- all_model_val_helper.sh [ ./backup_LMS/New-Processed-BPE-NormalText-gpt-cz-poetry-all-e4e16_LM ]
