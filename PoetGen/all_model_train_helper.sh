d80986b309f241d9106ee087aab7624e4b54b94c
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-07 20:32:59 +0100

    Model train

--- PoetGen/all_model_train_helper.sh
+++ PoetGen/all_model_train_helper.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#PBS -q gpu -l select=1:ncpus=1:ngpus=1:mem=24gb:scratch_local=16gb
-#PBS -l walltime=24:00:00
 module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
 cd $SCRATCHDIR
@@ -8,4 +6,4 @@ pip install --target=$SCRATCHDIR torch torchvision torchaudio --index-url https:
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen
-python3 model_validator.py --num_runs=5 --num_samples=200 --model_path_full="$1"
+python3 lm_finetune_torch_trainer_api.py --epochs_LM=$1 --epochs_poet=$2 --tokenizer="$3" --model_type="$4" --model_path="$5"
