5a01f4d2b86e8ef7c36897e5be59635d9790bc95
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-08 09:56:32 +0100

    train validator

---
+++ PoetGen/all_val_train.sh
@@ -0,0 +1,6 @@
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=32, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=32, TOK=ufal/robeczech-base, SYLAB=false'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=32,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=32, TOK=ufal/robeczech-base, SYLAB=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=32, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=32,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false'  all_val_train_helper.sh 
