4671babb299695e596f7ab0f11e966e888f55f75
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-12 11:19:49 +0100

    just error in meta...

--- PoetGen/all_val_train.sh
+++ PoetGen/all_val_train.sh
@@ -10,27 +10,27 @@ qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:sc
 qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=distilroberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
 qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=distilroberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh
 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
-#
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh
-#
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=roberta-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=roberta-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=roberta-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
-#
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=roberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh
-#
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=roberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
-#qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh
+
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=ufal/robeczech-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
+
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=roberta-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=roberta-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=roberta-base, SYLAB=true, SAM=true'  all_val_train_helper.sh 
+
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=roberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh
+
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=roberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=16, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=16,  EPRHYME=0, EPYEAR=0, TOK=roberta-base, SYLAB=false, SAM=true'  all_val_train_helper.sh 
 
