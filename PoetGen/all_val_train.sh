8f8968c4ed80a20894214548572842b4a8a2b0f3
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-11-24 16:44:12 +0100

    simple err

--- PoetGen/all_val_train.sh
+++ PoetGen/all_val_train.sh
@@ -2,7 +2,7 @@
 
 qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=distilroberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
 
-sub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
+qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=ufal/robeczech-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
 
 qsub -N TrainVal -q gpu_dgx -l select=1:ncpus=4:ngpus=1:mem=40gb:gpu_mem=30gb:scratch_local=64gb -l walltime=300:00:00 -v 'EPMETER=0,  EPRHYME=0, EPYEAR=16, TOK=roberta-base, SYLAB=false, SAM=false'  all_val_train_helper.sh 
 
