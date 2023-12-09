acb4de4f69e40b514f07dc469c94e1a3bff80fd7
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-12-09 20:58:21 +0100

    empty line removal

--- PoetGen/corpus_capsulated_datasets.py
+++ PoetGen/corpus_capsulated_datasets.py
@@ -613,6 +613,8 @@ class CorpusDatasetPytorch:
                 ]
             if "metre_ids" in batch[0].keys():
                 metre += [TextAnalysis._metre_vector(one_metre) for one_metre in datum['metre_ids']]
+        while "" in data_ids:
+            data_ids.remove("")
                 
         tokenized = tokenizer(data_ids, return_tensors='pt', truncation=True, padding=True)
         input_ids = tokenized['input_ids']
