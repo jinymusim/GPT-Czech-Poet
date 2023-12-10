482071ca865905aee182d2a5a651d91e90595059
Michal Chudoba <michal.chudoba.praha@gmail.com>
2023-12-10 20:33:22 +0100

    48 32

--- PoetGen/lm_finetune_torch_trainer_api.py
+++ PoetGen/lm_finetune_torch_trainer_api.py
@@ -25,9 +25,9 @@ from utils.poet_utils import EOS, PAD, UNK, parse_boolean
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--batch_size_LM", default=64, type=int, help="Batch size.")
+parser.add_argument("--batch_size_LM", default=48, type=int, help="Batch size.")
 parser.add_argument("--epochs_LM", default=0, type=int, help="Number of epochs to run.")
-parser.add_argument("--batch_size_poet", default=48, type=int, help="Batch size.")
+parser.add_argument("--batch_size_poet", default=32, type=int, help="Batch size.")
 parser.add_argument("--epochs_poet", default=0, type=int, help="Number of epochs for poet gen")
 parser.add_argument("--learning_rate", default=5e-5, type=float, help="Learning Rate for Finetuning")
 parser.add_argument("--train_masked", default=False, type=bool, help="Train for consistency secondary training")
