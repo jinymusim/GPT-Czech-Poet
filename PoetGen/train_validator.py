c58b884bba0d7c00018b4130d746311107ad2d87
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-01-05 10:24:04 +0100

    proper names

--- PoetGen/train_validator.py
+++ PoetGen/train_validator.py
@@ -322,7 +322,7 @@ def main(args):
     # Store result and model
     with open(args.result_file, 'a') as file:
         print("\n",file=file)
-        print(f"### LESS RHYME NEW NEW NEW FORMAT! ### {type(tokenizer.backend_tokenizer.model).__name__} ### {time_stamp} ### Syllable: {str(args.syllables)} ### SAM Training: {str(args.SAM)}", file=file)
+        print(f"### LESS RHYME NEW NEW NEW FORMAT! ### {tok_name} ### {time_stamp} ### Syllable: {str(args.syllables)} ### SAM Training: {str(args.SAM)}", file=file)
         print(f"Rhyme Validator: {args.pretrained_model}, Epochs: {args.epochs_rhyme} Accuracy: {rhyme_acc}", file=file)
         print(f'{rhyme_val_acc}', file=file)
         print(f"Metre Validator: {args.pretrained_model}, Epochs: {args.epochs_metre} Accuracy: {metre_acc}", file=file)
