a1dccb7f3acb2eddc2306af2d5822cea5068a42a
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-02-27 13:51:22 +0100

    small err in name

--- PoetGen/shell_scripts/perplexity_measure_helper.sh
+++ PoetGen/shell_scripts/perplexity_measure_helper.sh
@@ -6,4 +6,4 @@ pip install --target=$SCRATCHDIR torch torchvision torchaudio --index-url https:
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen
-python3 perplexity_com.py.py --model_path_full=$MODEL
+python3 perplexity_com.py --model_path_full=$MODEL
