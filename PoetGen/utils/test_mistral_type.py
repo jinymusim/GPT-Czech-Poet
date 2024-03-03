cb790de2092a5052fb173d9ee2d3de04b1e0d6ef
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-03 19:15:09 +0100

    70b llama

--- PoetGen/utils/test_mistral_type.py
+++ PoetGen/utils/test_mistral_type.py
@@ -22,7 +22,8 @@ parser = argparse.ArgumentParser()
 # Open-Orca/Mistral-7B-OpenOrca
 
 # mistralai/Mixtral-8x7B-Instruct-v0.1
-parser.add_argument("--model", default='mistralai/Mixtral-8x7B-Instruct-v0.1', type=str, help='Huggingface model id')
+# jarradh/llama2_70b_chat_uncensored
+parser.add_argument("--model", default='jarradh/llama2_70b_chat_uncensored', type=str, help='Huggingface model id')
 parser.add_argument("--data_path",  default=os.path.abspath(os.path.join(os.path.dirname(__file__),'..', "corpusCzechVerse", "ccv-new")), type=str, help="Path to Data")
 
 if __name__ == '__main__':
