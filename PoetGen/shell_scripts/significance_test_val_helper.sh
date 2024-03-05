bb738be3edea6e989366f1508a3d1301931052ef
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-05 09:43:05 +0100

    upgrade pip

--- PoetGen/shell_scripts/significance_test_val_helper.sh
+++ PoetGen/shell_scripts/significance_test_val_helper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
+python3 -m pip install --upgrade pip
 export TMPDIR=$SCRATCHDIR
 cd $SCRATCHDIR
 pip install --target=$SCRATCHDIR torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
