57f214ebfd8a88cc9ddb699adccb3fa99633f468
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-06 13:00:56 +0100

    old cuda try

--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-module add python/3.10.4-gcc-8.3.0-ovkjwzd
-python3 -m ensurepip --upgrade
+module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
 export CMAKE_ARGS="-DLLAMA_CUBLAS=on"
 export FORCE_CMAKE=1
 cd $SCRATCHDIR
-export PATH="${PATH}:/storage/brno12-cerit/home/chudobm/.local/bin"
-python3 -m pip install --upgrade pip
-pip install --target=$SCRATCHDIR --force-reinstall torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install --target=$SCRATCHDIR  torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install --target=$SCRATCHDIR --upgrade --force-reinstall llama-cpp-python --no-cache-dir
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
