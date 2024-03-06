3ba6a8758c1ec3d81576c9f3e81ffb23a3fb60aa
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-06 11:03:12 +0100

    force reinstall

--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -5,6 +5,7 @@ export CMAKE_ARGS="-DLLAMA_CUBLAS=on"
 export FORCE_CMAKE=1
 cd $SCRATCHDIR
 pip install --target=$SCRATCHDIR torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install --upgrade --force-reinstall llama-cpp-python --no-cache-dir
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/utils
