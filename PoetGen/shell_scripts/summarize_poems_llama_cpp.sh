b96e73f1e39e6997c567bac02c89cb33d3df84b3
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-06 15:02:22 +0100

    still no gpu

--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
-export CMAKE_ARGS="-DLLAMA_CUBLAS=on"
-export FORCE_CMAKE=1
 cd $SCRATCHDIR
 pip install --target=$SCRATCHDIR  torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install --target=$SCRATCHDIR  llama-cpp-python
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install --target=$SCRATCHDIR  llama-cpp-python --upgrade --force-reinstall --no-cache-dir
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/utils
