d72cc9c1cb71f43fd01c3f7c5328e23653504d6e
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-12 21:39:17 +0100

    This should be enough

--- PoetGen/shell_scripts/summarize_poems.sh
+++ PoetGen/shell_scripts/summarize_poems.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
-export CMAKE_ARGS="-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS" 
 cd $SCRATCHDIR
 pip install --target=$SCRATCHDIR torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 module add python/3.10.4-gcc-8.3.0-ovkjwzd
 export TMPDIR=$SCRATCHDIR
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" python -m pip install --target=$SCRATCHDIR  llama-cpp-python
+python -m pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" python -m pip install --target=$SCRATCHDIR  llama-cpp-python --upgrade 
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/utils
 python test_sumarization_llama_cpp.py
