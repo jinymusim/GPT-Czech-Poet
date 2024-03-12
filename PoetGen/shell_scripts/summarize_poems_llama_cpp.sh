242d046850a9584b73db1ea8a9b3ba92daf56e75
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-12 21:24:26 +0100

    No need for torch and other stuff here

--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 module add python/3.10.4-gcc-8.3.0-ovkjwzd
 export TMPDIR=$SCRATCHDIR
-python -m pip install --target=$SCRATCHDIR  torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-python -m pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" python -m pip install --target=$SCRATCHDIR  llama-cpp-python --no-cache-dir
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" python -m pip install --target=$SCRATCHDIR  llama-cpp-python
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/utils
 python test_sumarization_llama_cpp.py
