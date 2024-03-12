8a93717072b78ef303d734dfb906269d9a675648
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-12 21:47:45 +0100

    old pip

--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-module add python/3.10.4-gcc-8.3.0-ovkjwzd
+module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
-python -m pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" python -m pip install --target=$SCRATCHDIR  llama-cpp-python --upgrade 
+pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
+CMAKE_ARGS="-DLLAMA_CUBLAS=on"  pip install --target=$SCRATCHDIR  llama-cpp-python --upgrade 
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/utils
 python test_sumarization_llama_cpp.py
