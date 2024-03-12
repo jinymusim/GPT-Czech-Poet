392dddf370d0dc3fb28487eca760beabc3875063
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-12 21:15:24 +0100

    no cache dir

--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 module add python/3.10.4-gcc-8.3.0-ovkjwzd
 export TMPDIR=$SCRATCHDIR
-python -m pip install --target=$SCRATCHDIR  torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 python -m pip install --target=$SCRATCHDIR  llama-cpp-python --no-cache-dir
-python -m pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt  
+python -m pip install --target=$SCRATCHDIR  torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+python -m pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" python -m pip install --target=$SCRATCHDIR  llama-cpp-python --no-cache-dir
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/utils
 python test_sumarization_llama_cpp.py
