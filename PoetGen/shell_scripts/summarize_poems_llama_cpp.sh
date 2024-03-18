e87a9180f8e5831cc9f8d0b4022f53a38d66c919
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-03-19 00:22:11 +0100

    should work with assistant

--- PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
+++ PoetGen/shell_scripts/summarize_poems_llama_cpp.sh
@@ -3,7 +3,7 @@ module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR
 pip install --target=$SCRATCHDIR  torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install --target=$SCRATCHDIR llama-cpp-python=0.2.55 --no-cache-dir --force-reinstall
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install --target=$SCRATCHDIR llama-cpp-python --no-cache-dir --force-reinstall
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/utils
 python test_sumarization_llama_cpp.py
