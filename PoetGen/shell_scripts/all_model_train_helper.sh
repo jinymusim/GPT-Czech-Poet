a73333328218d791e4a95c83ca64f26689f275a5
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-10-18 20:10:42 +0200

    newer cuda

--- PoetGen/shell_scripts/all_model_train_helper.sh
+++ PoetGen/shell_scripts/all_model_train_helper.sh
@@ -2,7 +2,7 @@
 module add python/3.10.4-gcc-8.3.0-ovkjwzd
 export TMPDIR=$SCRATCHDIR 
 cd $SCRATCHDIR
-pip install --target=$SCRATCHDIR torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install --target=$SCRATCHDIR torch torchvision torchaudio
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen
