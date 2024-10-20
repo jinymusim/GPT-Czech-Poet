43f3a55f016f9b7de778753804ce9a4c3374d57d
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-10-20 08:17:23 +0200

    flash-att

--- PoetGen/shell_scripts/all_model_train_helper.sh
+++ PoetGen/shell_scripts/all_model_train_helper.sh
@@ -3,8 +3,8 @@ module add py-pip/21.3.1-gcc-10.2.1-mjt74tn
 export TMPDIR=$SCRATCHDIR 
 cd $SCRATCHDIR
 pip install --target=$SCRATCHDIR torch torchvision torchaudio
-pip install --target=$SCRATCHDIR flash-attn --no-build-isolation
 pip install --target=$SCRATCHDIR -r /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen/requirements.txt
+pip install --target=$SCRATCHDIR flash-attn --no-build-isolation
 export PYTHONPATH="${PYTHONPATH}:${TMPDIR}"
 cd /storage/brno2/home/chudobm/tf_shorts/Tensorflow-Shorts/PoetGen
 python3 lm_finetune_torch_trainer_api.py  --epochs_poet=$EPOCHSPOET --tokenizer="$TOKENIZER" --model_type="$MODELTYPE" --model_path="$MODEL" --default_hf_model="$HFMODEL"
